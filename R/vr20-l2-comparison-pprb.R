206e7598d0ad4d3e4ec4511a680324d34135607f
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2024-12-17 12:29:14 +0100

    Comparing L2 and Vx20

--- R/vr20-l2-comparison-pprb.R
+++ R/vr20-l2-comparison-pprb.R
@@ -22,11 +22,6 @@ las_file_vx20 <- "/media/ghislain/Agathis/20241212-YS-PPRB-P1-test/export/202412
 out_dir_ssd <- "/media/ghislain/Agathis/outputs/l2-vx20-pprb"
 dir.create(out_dir_ssd, showWarnings=FALSE)
 
-# PPRB bounding box
-ifile <- "/home/ghislain/Documents/Research_current/PROJECTS/MELANOBS/Data/PPRB/parcelle_pprb_p1.gpkg"
-pprb <- sf::read_sf(ifile)
-bbox_pprb <- sf::st_bbox(pprb)
-
 # ========================================
 # CHM with DJI-L2
 # ========================================
@@ -86,24 +81,38 @@ p <- ggplot(payload(las_tr), aes(X, Z, color = Z)) +
   scale_color_gradientn(colours = height.colors(50))
 ggsave(file.path(out_dir_ssd, "transect.png"), p, width=10, height=2)
 
+# Chunk
+opt_chunk_size(ctg) <- 160
+plot(ctg, chunk=TRUE)
+
+# Classifying points
+ofile <- file.path(out_dir_ssd, "ys-vx20-classified.las")
+pipeline <- reader_las() +
+  classify_with_csf(
+    slope_smooth=TRUE,
+    class_threshold=0.5,
+    cloth_resolution=0.5) +
+  write_las(ofile)
+exec(pipeline, on=ctg)
+
 # Plot ground points
+las_vx20_pprb <- readLAS(ofile, select="xyzc")
+print(las_vx20_pprb)
+p1 <- c(470977, 232806)
+p2 <- c(470999, 232667)
+las_tr <- clip_transect(las_vx20_pprb, p1, p2, width=2, xz=TRUE)
 p <- ggplot(payload(las_tr), aes(X, Z, color = Classification)) + 
   geom_point(size = 0.5) + 
   coord_equal() + 
   theme_minimal()
 ggsave(file.path(out_dir_ssd, "tr-ground-points.png"), p, width=10, height=2)
-# No ground points !
-
-# No chunk here
-## opt_chunk_size(ctg) <- 250
-## plot(ctg, chunk=TRUE)
 
 # DTM and DSM
 pipeline <- reader_las() +
   chm(1, ofile=file.path(out_dir_ssd, "dsm_vx20.tif")) +
   dtm(1, ofile=file.path(out_dir_ssd, "dtm_vx20.tif"))
 set_exec_options(progress=TRUE, ncores=sequential())
-exec(pipeline, on=ctg)
+exec(pipeline, on=ofile)
 
 # CHM
 dsm <- terra::rast(file.path(out_dir_ssd, "dsm_vx20.tif"))
@@ -121,5 +130,52 @@ terra::writeRaster(
   overwrite=TRUE,
   NAflag=255)
 
+# ========================================
+# CHM comparison at 1m resolution
+# ========================================
+
+
+
+
+# ========================================
+# CHM comparison at 10m resolution
+# ========================================
+
+# PPRB P1 plot
+source("~/Code/forest-plot-grid/forest-plot-grid.R")
+get_grid(coords=c(166.688126, -22.106773),
+         azimut=306,
+         proj="epsg:3163",
+         distance=100,
+         bin=10,
+         output_dir=out_dir_ssd)
+ifile <- file.path(out_dir_ssd, "grid.gpkg")
+p1_pprb <- sf::st_read(ifile, quiet=TRUE) |>
+  sf::st_transform(crs="epsg:3163")
+
+# Zonal statistics in subplot
+chm_l2 <- terra::rast(file.path(out_dir_ssd, "chm_l2.tif"))
+chm_vx20 <- terra::rast(file.path(out_dir_ssd, "chm_vx20.tif"))
+p1_pprb_l2 <- terra::zonal(
+  x=chm_l2,
+  z=terra::vect(p1_pprb),
+  fun="mean", na.rm=TRUE,
+  as.polygons=TRUE)
+p1_pprb_l2_vx20 <- terra::zonal(
+  x=chm_vx20,
+  z=p1_pprb_l2,
+  fun="mean", na.rm=TRUE,
+  as.polygons=TRUE)
+df <- as.data.frame(p1_pprb_l2_vx20) |>
+  dplyr::rename(subplot=name, height_l2=2, height_vx20=3)
+
+# Plotting
+p <- ggplot(df, aes(x=height_l2, y=height_vx20)) +
+  geom_point() +
+  xlab("Mean canopy height (m), DJI-L2, 10 x 10 m") +
+  ylab("Mean canopy height (m), YS-Vx20, 10 x 10 m") +
+  geom_abline(slope=1, intercept=0, colour="red") +
+  theme_bw()
+ggsave(file.path(out_dir_ssd, "chm_comp.png"), p)
 
 # End
