41381e7c585a0bf7a5e02b4599889861348b14a2
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2025-06-02 16:24:50 +1100

    Make map of amap lidar sites

--- R/lidar-sites.R
+++ R/lidar-sites.R
@@ -11,14 +11,15 @@ if(!("lasR" %in% installed_packages)) {
 # Load libraries
 library(lidR)
 library(lasR)
-library(lidRviewer)
+#library(lidRviewer)
 library(here)
 library(glue)
 library(sf)
 library(terra)
-library(ggplot2)
+#library(ggplot2)
 library(readr)
 library(dplyr)
+library(tmap)
 
 # The Agathis folder
 # Agathis folder in project directory is a symlink to /media/ghislain/Agathis
@@ -42,15 +43,15 @@ for (i in 1:nsites) {
   site <- lidar_df$site[i]
   ofile <- here(out_dir_ssd, glue("{site}.gpkg"))
   # Only execute if GPKG file does not exist
-  if (!file.exists(ofile) & site != "dzumac-2") {
+  if (!file.exists(ofile)) {
     # LAS files
     las_files <- lidar_df$path_las_files[i]
     if (grepl(";", las_files)) {
       las_files <- unlist(strsplit(las_files, split=";"))
     }
-    # Get catalogue of file
+    # Get catalogue of files
     ctg <- lidR::readLAScatalog(here("Agathis", las_files))
-    ## # Get boundaries using convex hull (not efficient)
+    ## # Get boundaries using convex hull (not efficient, use lasboundary)
     ## ctg_boundaries <- lidR::catalog_boundaries(
     ##   ctg,
     ##   concavity=1,
@@ -75,27 +76,111 @@ for (i in 1:nsites) {
     cntrd <- sf::st_centroid(sf_files_union)
     cntrd_coord <- sf::st_coordinates(cntrd)
     area <- sf::st_area(sf_files_union)
-    # Add columns to data frame
+    # Filling in the data frame
     area <- round(as.numeric(area) / 10000, 2)
     X <- cntrd_coord[1]
     Y <- cntrd_coord[2]
     geom <- sf_files_union
     df[i, 2:4] <- c(X, Y, area)
     df$geometry[i] <- geom
-    ## sf_lidar_area <- sf::st_as_sf(
-    ##   lidar_data,
-    ##   geometry=sf_files_union)
-    ## # Export as GPKG file
-    ## write_sf(sf_lidar_area, ofile)
   }
 }
 
-# Create sf and export
-sf_lidar_sites <- sf::st_as_sf(df) |>
-  sf::st_set_crs("epsg:32758")
-
-# Export as GPKG file
+# Create sf and export as gpkg
 ofile <- here(out_dir_ssd, "lidar-sites.gpkg")
-write_sf(sf_lidar_sites, ofile)
+sf_lidar_sites <- sf::st_as_sf(df) |>
+  sf::st_set_crs("epsg:32758") |>
+  write_sf(ofile)
+
+# Create point data and export as gpkg
+ofile <- here(out_dir_ssd, "lidar-sites-pts.gpkg")
+df_pts <- df |>
+  select(-geometry) |>
+  sf::st_as_sf(
+    coords=c("X", "Y"),
+    remove=FALSE,
+    crs=sf::st_crs("epsg:32758")) |>
+  write_sf(ofile)
+
+# =====================================
+# Map of lidar sites
+# =====================================
+
+# Lidar sites
+ifile <- here(out_dir_ssd, "lidar-sites-pts.gpkg")
+lidar_sites <- sf::read_sf(ifile)
+
+# Borders from GADM
+ifile <- here("gisdata", "borders-newcal.gpkg")
+borders <- sf::read_sf(ifile) |>
+  sf::st_transform("epsg:32758")
+
+# Forest from TMFv2023
+ifile <- here("gisdata", "forest-tmf-2000-2010-2020.tif")
+ofile <- here("gisdata", "forest-newcal-tmf-2020.tif")
+gdal_utils(
+  util="warp",
+  source=ifile,
+  destination=ofile,
+  options=c(
+    "-b", "3",
+    "-tr", "500", "500",
+    "-t_srs", "EPSG:32758",
+    "-r", "mode",
+    "-srcnodata", "None",
+    "-dstnodata", "0",
+    "-ot", "Byte",
+    "-tap", "-overwrite",
+    "-co", "COMPRESS=DEFLATE"))
+forest <- terra::rast(ofile)
+
+# Options
+tmap_opt <- function(npix=1e5, ...) {
+  tmap_options_reset()
+  tmap_options(raster.max_cells = c(plot=npix, view=npix), ...)
+  cat("raster.max_cells set to: ", npix, "\n")
+}
+tmap_opt(npix=1e7)
+green <- rgb(34, 139, 34, 255, maxColorValue=255)
+ofile <- here(out_dir_ssd, "lidar-sites.png")
+textwidth <- 16.6  # textwidth (in cm) for figure width
+
+# Map
+tm_lidar <- 
+  tm_shape(forest) +
+  tm_raster(
+    col.scale=tm_scale_categorical(values=c(green)),
+    col_alpha=0.5,
+    col.legend=tm_legend(
+      # frame=FALSE, # not working
+      frame.lwd=0,
+      text.color="transparent",
+      title="Forest cover in 2020\n(source TMFv2023)", 
+      #orientation="landscape",
+      position=tm_pos_in(0.02, 0.24, just.h="left", just.v="bottom"))
+  ) +
+  tm_shape(borders, unit="km") +
+  tm_borders(col="black", lwd=0.75) +
+  tm_scalebar(
+    breaks=c(0, 50, 100), text.size=0.7,
+    position=tm_pos_in(0.5, 1, just.h="center", just.v="top")
+  ) +
+  tm_shape(lidar_sites) +
+  tm_squares(
+    col="blue", fill="white", fill_alpha=0, size="area",
+    size.legend=tm_legend(
+      frame=FALSE,
+      title="Area (ha)", 
+      orientation="landscape",
+      position=tm_pos_in(0.02, 0.08, just.h="left", just.v="bottom"))
+  ) +
+  tm_layout(inner.margins=rep(0.02, 4),
+            outer.margins=rep(0.02, 4)) +
+  tm_title(text="AMAP lidar sites in New Caledonia",
+           position=tm_pos_in(0.02, 0.02, just.h="left", just.v="bottom"),
+           size=1.2)
+
+# Save map
+tmap_save(tm_lidar, filename=ofile, width=textwidth, units="cm", dpi=300, scale=1)
 
 # End
